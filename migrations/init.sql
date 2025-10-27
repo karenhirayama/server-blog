7ef76e6e4cd5089533d476e26115f9ac751ad042
karenhirayama <karenm.hirayama@gmail.com>
2025-10-27 10:56:37 -0300

    feat: Add init sql

---
+++ migrations/init.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
