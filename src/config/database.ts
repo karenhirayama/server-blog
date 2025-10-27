60f67c205e1d2cd3b32f62880b9f321e2e764c71
karenhirayama <karenm.hirayama@gmail.com>
2025-10-27 10:57:23 -0300

    feat: Create database config

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+import { ENV } from "./env";
+
+export const pool = new Pool({
+  connectionString: ENV.DATABASE_URL,
+});
+
+pool.connect(() => {
+  console.log("Connected to the database");
+})
+
+pool.on('error', (err) => {
+  console.error('Database connection error:', err);
+})
+
