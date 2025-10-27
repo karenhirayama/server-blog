c040ea8ad58fa1da7cac5a52c643dc593be9c810
karenhirayama <karenm.hirayama@gmail.com>
2025-10-27 10:57:38 -0300

    feat: Add env to config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,11 @@
-import { config } from "dotenv";
+import { config } from 'dotenv'
 
 config()
 
 export const ENV = {
-    PORT: process.env.PORT
+  PORT: process.env.PORT || 3000,
+  DATABASE_URL: process.env.DATABASE_URL || '',
+  JWT_SECRET: process.env.JWT_SECRET || '',
+  JWT_EXPIRES_IN: Number(process.env.JWT_EXPIRES_IN) || (60 * 60 * 24),
+  NODE_ENV: process.env.NODE_ENV || 'development',
 }
